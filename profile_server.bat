39fadeb61b09928b564124f51dd1486460d40e81
Nounours31 <internet.fages@free.fr>
2021-07-16 08:54:22 +0200

    Update profile_server.bat

---
+++ profile_server.bat
@@ -0,0 +1,7 @@
+echo from E:\WS\GitHubPerso\FicheValerie
+
+call E:\WS\GitHubPerso\FicheValerie\server\v1.0.0\graddle.bat
+echo gradle installDist --warning-mode all --info
+
+gradle installDist --warning-mode all --info
+
