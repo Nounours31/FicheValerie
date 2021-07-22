eb2b2c0fd09b391e9b300e7edb8baf5afdf9d03b
Nounours31 <internet.fages@free.fr>
2021-07-22 22:32:19 +0200

    Update sfa_fichevalerie_2021-07-22.sql and cEnv.ts

--- DB/sfa_fichevalerie_2021-07-22.sql
+++ DB/sfa_fichevalerie_2021-07-22.sql
@@ -40,15 +40,11 @@ CREATE TABLE IF NOT EXISTS `activite` (
   `date` datetime NOT NULL,
   `tarifHoraire` float NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=47 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+) ENGINE=MyISAM AUTO_INCREMENT=1 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 --
 -- Dumping data for table `activite`
 --
-
-INSERT INTO `activite` (`id`, `idBulletinSalaire`, `activitee`, `gmtepoch_debut`, `gmtepoch_fin`, `date`, `tarifHoraire`) VALUES
-(46, 9, 'pêche', 1625126400000, 1625130000000, '2021-07-22 20:47:49', -1),
-(44, 9, 'Jeux', 1625126400000, 1625137200000, '2021-07-22 10:21:46', -1);
 
 -- --------------------------------------------------------
 
@@ -65,14 +61,11 @@ CREATE TABLE IF NOT EXISTS `bulletinsalaire` (
   `tarifHoraire` float NOT NULL,
   `date` datetime NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=10 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+) ENGINE=MyISAM AUTO_INCREMENT=1 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 --
 -- Dumping data for table `bulletinsalaire`
 --
-
-INSERT INTO `bulletinsalaire` (`id`, `idPersonne`, `mois`, `annee`, `tarifHoraire`, `date`) VALUES
-(9, 1, 6, 2021, 15, '2021-07-22 10:04:50');
 
 -- --------------------------------------------------------
 
@@ -88,15 +81,11 @@ CREATE TABLE IF NOT EXISTS `depassementforfaitaire` (
   `date` datetime NOT NULL,
   `tarifHoraire` float NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=5 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+) ENGINE=MyISAM AUTO_INCREMENT=1 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 --
 -- Dumping data for table `depassementforfaitaire`
 --
-
-INSERT INTO `depassementforfaitaire` (`id`, `idBulletinSalaire`, `dureeenheure`, `date`, `tarifHoraire`) VALUES
-(3, 8, 0.5, '2021-07-22 10:00:36', -1),
-(4, 9, 0.5, '2021-07-22 10:05:29', -1);
 
 -- --------------------------------------------------------
 
@@ -131,19 +120,11 @@ CREATE TABLE IF NOT EXISTS `listactivitee` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `nom` text COLLATE utf8_bin NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=11 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+) ENGINE=MyISAM AUTO_INCREMENT=1 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 --
 -- Dumping data for table `listactivitee`
 --
-
-INSERT INTO `listactivitee` (`id`, `nom`) VALUES
-(7, 'pÃªche'),
-(5, 'PÃªche'),
-(4, 'Jeux'),
-(8, 'p%C3%AAche'),
-(9, 'pÃªche'),
-(10, 'pêche');
 
 -- --------------------------------------------------------
 
@@ -159,20 +140,11 @@ CREATE TABLE IF NOT EXISTS `pdf` (
   `version` int(11) NOT NULL,
   `date` datetime NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=19 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+) ENGINE=MyISAM AUTO_INCREMENT=1 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 --
 -- Dumping data for table `pdf`
 --
-
-INSERT INTO `pdf` (`id`, `idBulletinSalaire`, `file`, `version`, `date`) VALUES
-(18, 9, '9\\02\\0220210e-f796-470d-9257-caa9920a8fe5.pdf', 7, '2021-07-22 20:48:15'),
-(17, 9, '9\\03\\031875ce-ca10-4553-aba0-f834820f849a.pdf', 6, '2021-07-22 20:42:11'),
-(16, 9, '9\\0b\\0b548436-37bc-473b-a451-de5c4861eedb.pdf', 5, '2021-07-22 12:53:33'),
-(15, 9, '9\\d4\\d4a72c38-8fa0-492f-8e4a-fd86989067da.pdf', 4, '2021-07-22 12:49:11'),
-(14, 9, '9\\bb\\bbc7b5a6-66d8-4506-8cb7-bae561d02127.pdf', 3, '2021-07-22 12:45:26'),
-(13, 9, '9\\06\\0621d91f-114f-46a0-8c02-71efdc346ab6.pdf', 2, '2021-07-22 12:44:29'),
-(12, 9, '9\\b9\\b9140d8a-45d5-461e-b8bb-1213fadf4b52.pdf', 1, '2021-07-22 10:21:57');
 
 -- --------------------------------------------------------
 
@@ -187,15 +159,7 @@ CREATE TABLE IF NOT EXISTS `personne` (
   `nom` text COLLATE utf8_bin NOT NULL,
   `date` datetime NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
-
---
--- Dumping data for table `personne`
---
-
-INSERT INTO `personne` (`id`, `genre`, `nom`, `date`) VALUES
-(1, 'Madame', 'Beautée', '2021-07-21 19:08:55'),
-(2, 'Monsieur', 'ùéèàê', '2021-07-22 11:19:01');
+) ENGINE=MyISAM AUTO_INCREMENT=1 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 -- --------------------------------------------------------
 
@@ -213,15 +177,8 @@ CREATE TABLE IF NOT EXISTS `rappel` (
   `date` datetime NOT NULL,
   `status` tinyint(4) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=5 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+) ENGINE=MyISAM AUTO_INCREMENT=1 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
---
--- Dumping data for table `rappel`
---
-
-INSERT INTO `rappel` (`id`, `idBulletinSalaire`, `idBulletinSalaireOrigine`, `dureeenheure`, `tarifHoraire`, `date`, `status`) VALUES
-(3, 8, -1, 14.75, -1, '2021-07-22 10:00:36', 1),
-(4, 9, -1, 14.75, -1, '2021-07-22 10:05:29', 1);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
--- ui/v1.0.0/src/WS/cEnv.ts
+++ ui/v1.0.0/src/WS/cEnv.ts
@@ -2,5 +2,5 @@ export default class cEnv {
     private static _serverURLDell: string = "http://localhost:8080/ficheValerie-2021-07-01/v1.0.0";
     private static _serverURLVape: string = "http://localhost:8090/ficheValerie-2021-07-01/v1.0.0";
     
-    public static _serverURL: string = cEnv._serverURLDell;
+    public static _serverURL: string = cEnv._serverURLVape;
 }
