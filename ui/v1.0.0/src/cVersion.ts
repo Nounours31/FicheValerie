ec69a9bc38bc51889c6148560b86dfae05b52739
Nounours31 <internet.fages@free.fr>
2021-07-26 01:03:54 +0200

    Update cVersion.ts

--- ui/v1.0.0/src/cVersion.ts
+++ ui/v1.0.0/src/cVersion.ts
@@ -6,13 +6,13 @@ export default class cVersion {
 
     public setDateBuild(): string {
         let retour = "";
-        retour += "Build Date: 2021/07/26 00:58:27";
+        retour += "Build Date: 2021/07/26 01:01:50";
         return retour;
     }
 
     public setCommitVersionBuild(): string {
         let retour = "";
-        retour += "Commit Id: 5ef35f8725cf2fd769158f59148d89b5287bd462 - Commit Date:    Mon Jul 26 00:58:14 2021 +0200 - Commit branch: ";
+        retour += "Commit Id: 04ac4f7779cd1d8de358c61964dcaa7618c95d22 - Commit Date:    Mon Jul 26 00:59:48 2021 +0200 - Commit branch: ";
         return retour;
     }
 
