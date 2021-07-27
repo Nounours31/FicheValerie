62fee1b9ccaeb02f02ed42b008821a68fea6001b
Nounours31 <internet.fages@free.fr>
2021-07-27 20:37:18 +0200

    Update E4ABuildVersion.java, cEnv.ts, and cVersion.ts

--- server/v1.0.0/lib/src/main/java/sfa/fichevalerie/tools/E4ABuildVersion.java
+++ server/v1.0.0/lib/src/main/java/sfa/fichevalerie/tools/E4ABuildVersion.java
@@ -4,7 +4,7 @@ public class E4ABuildVersion {
 	public E4ABuildVersion() {}
 	
 	public String getBuildVersion() {
-		String retourTAGGED="2021/07/26 16:49:33";
+		String retourTAGGED="2021/07/27 20:33:06";
 		return retourTAGGED;
 	}
 }
