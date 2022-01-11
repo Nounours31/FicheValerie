aaadedac111e2d29da00a9bf039cfa64ed25df6a
Pierre Nounours Fages <pierre.fages@3ds.com>
2022-01-11 11:34:51 +0100

    Dev en cours

--- V1/OutilsSalaireMams/CreateAnnee.sh
+++ V1/OutilsSalaireMams/CreateAnnee.sh
@@ -3,31 +3,28 @@
 typeset -i test=1
 
 if ((test == 1)); then
-	To=/e/WorkSpaces/WS/GitHub/FicheValerie/V1/test
-	Ref=/e/WorkSpaces/WS/GitHub/FicheValerie/V1/OutilsSalaireMams/Ref.xlsx
+	To=/e/WorkSpaces/WS/GitHub/FicheValerie/V1/OutilsSalaireMams/test
+	Ref=/e/WorkSpaces/WS/GitHub/FicheValerie/V1/OutilsSalaireMams/Ref.xlsm
 else
 	To=/d/mams/BulletinsSalaire
-	Ref=/d/PAPA/OutilsSalaireMams/Ref.xlsx
+	Ref=/d/PAPA/OutilsSalaireMams/Ref.xlsm
 fi
 XLS='/c/Program Files (x86)/Microsoft Office/root/Office16/EXCEL.EXE'
 
-typeset -a Mois=(Janvier Fevrier Mars Avril Mai Juin Juillet Aout Septembre Octobre Novembre Decembre)
-typeset -a Persone=("Barrois Cecile" Govin Martiny StJalm Vieira Nouvelle)
+typeset -a Mois=(Janvier Fevrier Mars Avril Mai Juin Juillet Aout Septembre Octobre Novembre Decembre test)
 
-printf "Annee ?"
+printf "Annee ? "
 
 read annee
 echo $annee
 
 mkdir -p $To/$annee
-for ((i = 0; i < 12; i++)); do
+for ((i = 0; i < ${#Mois[@]}; i++)); do
 	mois=$(printf "%02d" "$((i+1))")
 	Rep="$To/$annee/${mois}-${annee}-${Mois[$i]}"
 	
 	mkdir $Rep
-	for ((j = 0; j < ${#Persone[@]}; j++)); do
-		cp $Ref "$Rep/$(basename $Ref)"
-	done
+	cp $Ref "$Rep/_$(basename $Ref)"
 done
 
 
---
+++ V1/OutilsSalaireMams/runMacro.bat
@@ -0,0 +1,2 @@
+
+"C:\Program Files (x86)\Microsoft Office\Office16\EXCEL.EXE" ".\Ref.xlsm"
